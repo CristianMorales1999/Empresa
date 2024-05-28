4efe7aadd8327b9c327532798e97284ffac09ab7
CristianMorales1999 <cm9225064@gmail.com>
2024-05-28 18:07:07 -0500

    Paso 02: Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,26 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
-Route::get('/login', function(){
-    return view('login');
-});
+Route::get('/servicios/{opcional?}', function ($opcional = null) {
+    return view('servicios', ['opcional' => $opcional]);
+})->where('opcional', '[A-Za-z]+')->name('servicios');
 
-Route::get('/logout', function(){
-    // cerrar sesión
-        return redirect('/'); 
-});
+Route::get('/proyectos/{opcional?}', function ($opcional = null) {
+    return view('proyectos', ['opcional' => $opcional]);
+})->where('opcional', '[A-Za-z]+')->name('proyectos');
 
-Route::get('/catalog', function(){
-    return view ('catalog.index');
-});
+Route::get('/clientes/{opcional?}', function ($opcional = null) {
+    return view('clientes', ['opcional' => $opcional]);
+})->where('opcional', '[A-Za-z]+')->name('clientes');
 
-Route::get('/catalog/show/{id}', function($id){
-    return view ('catalog.show',['id'=>$id]);
-});
+Route::get('/blog/{opcional?}', function ($opcional = null) {
+    return view('blog', ['opcional' => $opcional]);
+})->where('opcional', '[0-9]+')->name('blog');
 
-Route::get('/catalog/create', function(){
-    return view ('catalog.create');
-});
-
-Route::get('/catalog/edit/{id}', function ($id) {
- return view('catalog.edit', ['id' => $id]);
-});
+Route::get('/contacto', function () {
+    return view('contacto');
+})->name('contacto');
